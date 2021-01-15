b170e92f826e5e47708522bf34ef2832b822e431
Xamnalim <mlapinski98@gmail.com>
2021-01-15 13:58:49 +0100

    Added django-heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 import os
+import django_heroku
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -126,3 +128,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
